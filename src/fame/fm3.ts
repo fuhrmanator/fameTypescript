8287403928ff7dfe6e75617d5d19c102ed714cdd
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-08-05 11:52:27 -0400

    more FM3

--- src/fame/fm3.ts
+++ src/fame/fm3.ts
@@ -1,5 +1,10 @@
 export abstract class Element {
     name!: string
+    // abstract get fullname(): string
+    // abstract get owner(): Element
+    constructor(name: string) {
+        this.name = name
+    }
 }
 
 export class Package extends Element {
@@ -9,20 +14,36 @@ export class Package extends Element {
 
 export class Class extends Element {
     isAbstract: boolean
-    superclass: Class
+    superclass?: Class
     package: Package
-    properties: Property[]
-
+    properties: Property[] = []
+    get allProperties(): Property[] {
+        if (this.superclass) 
+            return this.properties.concat(this.superclass.allProperties)
+        else
+            return this.properties
+    }
+    get isPrimitive() {
+        return (this.name === "Number" || this.name === "String") //etc
+    }
+    get isRoot() {
+        return superclass == undefined
+    }
+    constructor(name: string, isAbstract: boolean = false, pkg: Package) {
+        super(name)
+        this.isAbstract = isAbstract
+        this.package = pkg
+    }
 }
 
 export class Property extends Element {
     class: Class
-    isContainer : boolean
-    isDerived : boolean
-    isKey : boolean
-    isMultivalued : boolean
+    isContainer: boolean
+    isDerived: boolean
+    isKey: boolean
+    isMultivalued: boolean
     opposite: Property
     package: Package
     type: Class
-    isComposite : boolean
+    isComposite(): boolean
 }
